3f4365976cc9445360deb5e284616ef4b9233c8f
MariLin587 <m.lindgren@elisanet.fi>
2016-01-24 18:54:30 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,21 @@
+
+## Presuming that the file is in the working directory
+
+## Reading the data
+powerConsumption<-read.table("household_power_consumption.txt",sep=";", header=TRUE)
+
+## Taking only Feb 1 and 2 2007 data
+
+powerConsumption2 <- powerConsumption[as.character(powerConsumption$Date) %in% c("1/2/2007", "2/2/2007"),]
+
+## Join date and time into one variable
+powerConsumption2$dateTime = paste(powerConsumption2$Date, powerConsumption2$Time)
+
+# Convert to Date/Time class
+powerConsumption2$dateTime <- strptime(powerConsumption2$dateTime, "%d/%m/%Y %H:%M:%S")
+attach(powerConsumption2)
+
+png("plot1.png", width=480, height=480, units="px")
+hist(as.numeric(as.character(Global_active_power)), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
